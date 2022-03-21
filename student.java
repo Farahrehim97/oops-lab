80089855db10e0e5132019179f0a9acd49f6967a
Farahrehim97 <95203723+Farahrehim97@users.noreply.github.com>
2022-03-20 22:05:10 -0700

    Add files via upload

---
+++ student.java
@@ -0,0 +1,14 @@
+package controlsttmnt;
+import java.util.*;
+public class student {
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter your name");
+	String name=sc.nextLine();
+	System.out.println("enter roll number");
+	int rno=sc.nextInt();
+	System.out.println("Name is"+name);
+	System.out.println("Roll No is"+rno);
+}
+}
